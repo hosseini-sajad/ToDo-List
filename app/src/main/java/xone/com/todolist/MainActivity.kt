44e604252988f01cd144525248051a2068510445
xone <xone_e@yahoo.com>
2021-04-06 22:01:37 +0100

    added TodoListFragment and AddTodoFragment and Themes

--- app/src/main/java/xone/com/todolist/MainActivity.kt
+++ app/src/main/java/xone/com/todolist/MainActivity.kt
@@ -2,10 +2,18 @@ package xone.com.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       /* val window: Window = this@MainActivity.window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.statusBarColor = ContextCompat.getColor(this@MainActivity, R.color.white_primary)*/
     }
 }
---
+++ app/src/main/java/xone/com/todolist/TodoListFragment.kt
@@ -0,0 +1,29 @@
+package xone.com.todolist
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import xone.com.todolist.databinding.FragmentTodolistBinding
+
+
+class TodoListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding : FragmentTodolistBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_todolist, container, false)
+
+        binding.viewRoot.setOnClickListener {
+            findNavController().navigate(TodoListFragmentDirections.actionTodoListFragmentToAddTodoFragment())
+        }
+
+        return binding.root
+    }
+}
