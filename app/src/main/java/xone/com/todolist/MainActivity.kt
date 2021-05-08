9ad61a832d1cd8cc1ddf2b9602cab7b0db4ee963
xone <hosseni_sajjad@yahoo.com>
2021-05-09 00:27:36 +0100

    Add navigation settings and fragment settings

--- app/src/main/java/xone/com/todolist/MainActivity.kt
+++ app/src/main/java/xone/com/todolist/MainActivity.kt
@@ -2,6 +2,7 @@ package xone.com.todolist
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import xone.com.todolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,9 +14,28 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        if (savedInstanceState == null) {
+            setupBottomNavigationView()
+        }
+
         /* val window: Window = this@MainActivity.window
          window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
          window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
          window.statusBarColor = ContextCompat.getColor(this@MainActivity, R.color.white_primary)*/
     }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        setupBottomNavigationView()
+    }
+
+    private fun setupBottomNavigationView() {
+        binding.bottomNavigationView.setupWithNavController(
+            navGraphIds = listOf(R.navigation.navigation_todolist, R.navigation.navigation_settings),
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.fragmentContainer,
+            intent = intent
+        ).observe(this, Observer {})
+    }
+
 }
